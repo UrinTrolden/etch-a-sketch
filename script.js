b2179c34e1e3e07a8ceead15a32b1ac11b2ba487
UrinTrolden <urintrolden@gmail.com>
2021-08-12 16:56:45 +0200

    reset button and grid now works as intended

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 const container = document.getElementById("container");
 
 let createGrid = function (rows, cols) {
-    //https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
+    
+    let grids = document.querySelectorAll('.grid-item');
+    grids.forEach((grid) => {
+        grid.remove();
+    });
+    
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
        };
+    gridColorer();
 };
 
 
 
-createGrid(16, 16);
+
 
 let grids = document.querySelectorAll('.grid-item');
 let resetBtn = document.getElementById("resetbtn");
 
-grids.forEach((grid) => {
-    grid.addEventListener('mouseover', () => {
+let gridColorer = () => {
+    
+    let grids = document.querySelectorAll('.grid-item');
+
+    grids.forEach((grid) => {
+        grid.addEventListener('mouseover', () => {
         grid.style.cssText = 'background: black';
     })
 });
+}
+
 
 resetBtn.onclick = () => {
     let rows = prompt("How many rows do you want?");
     let cols = prompt("How many collumns do you want?");
+    if ((cols || rows) > 100) {
+        return alert("Can't do more than 100 rows or collumns, try again.")
+    }
     return createGrid(rows, cols);
 }
 
+createGrid(16, 16);
+gridColorer();
