0547b590810d09579eebd1a5863613f624a5fe97
UrinTrolden <urintrolden@gmail.com>
2021-08-12 16:09:34 +0200

    drawing now works

--- script.js
+++ script.js
@@ -6,9 +6,26 @@ let createGrid = function (rows, cols) {
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item";
        };
 };
 
+
+
 createGrid(16, 16);
+
+let grids = document.querySelectorAll('.grid-item');
+let resetBtn = document.getElementById("resetbtn");
+
+grids.forEach((grid) => {
+    grid.addEventListener('mouseover', () => {
+        grid.style.cssText = 'background: black';
+    })
+});
+
+resetBtn.onclick = () => {
+    let rows = prompt("How many rows do you want?");
+    let cols = prompt("How many collumns do you want?");
+    return createGrid(rows, cols);
+}
+
